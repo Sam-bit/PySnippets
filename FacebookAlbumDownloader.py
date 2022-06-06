2c8dfa0b367a0154acee476188aac49c59a27419
Sambit Samal <itsme.sambit1991@gmail.com>
2022-06-06 11:11:57 +0530

    Update FacebookAlbumDownloader.py
    Added photo albums downloads

--- FacebookAlbumDownloader.py
+++ FacebookAlbumDownloader.py
@@ -3,6 +3,7 @@ import time,os
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 CHROME_DVR_DIR = 'C:\\drivers\\chromedriver.exe'
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,8 +13,9 @@ from pathlib import Path
 from urllib.request import urlretrieve
 LOGIN_URL = 'https://www.facebook.com/login.php'
 options = webdriver.ChromeOptions()
-options.add_argument("--user-data-dir=C:\\Users\\Sambit Samal\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+options.add_argument("--user-data-dir=C:\\Users\\<username>\\AppData\\Local\\Google\\Chrome\\User Data\\Default")#Change the username
 options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-infobars")
 PAUSE_TIME = 5
 downloadfolder = "E:\\facebookalbums"
 class FacebookLogin():
@@ -48,6 +50,72 @@ class FacebookLogin():
             if requests.get(actualimgsrc).status_code == 200:
                 print(f'Downloading {filename}')
                 urlretrieve(actualimgsrc, filename)
+    def get_album_photos(self, album_url, album_title, folderpath):
+        folderpath = f'{folderpath}{os.sep}{album_title}'
+        if not os.path.exists(folderpath):
+            print(f'Created Directory {folderpath}')
+            os.makedirs(folderpath)
+            time.sleep(2)
+        self.driver.get(album_url)
+        # Get scroll height
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+        while True:
+            # Scroll down to bottom
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # Wait to load page
+            time.sleep(PAUSE_TIME)
+            # Calculate new scroll height and compare with last scroll height
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                time.sleep(PAUSE_TIME)
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                if new_height == last_height:
+                    xpathroot = '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[2]/div/div'
+                    divs = self.driver.find_element(by=By.XPATH, value=xpathroot)
+                    element = divs.get_attribute('innerHTML')
+                    soup = BeautifulSoup(element,'html.parser')
+                    for elem in soup.find_all('div'):
+                        if elem.find('a'):
+                            self.download_image("https://www.facebook.com/"+elem.find('a')['href'],folderpath)
+                    break
+            last_height = new_height
+    def scroll_page_photos_albums(self, profile_id,downloadfolder):
+        folderpath = f'{downloadfolder}{os.sep}{profile_id}{os.sep}photos_albums'
+        if not os.path.exists(folderpath):
+            print(f'Created Directory {folderpath}')
+            os.makedirs(folderpath)
+            time.sleep(2)
+        photos_id_url = "https://www.facebook.com/"+profile_id+"/photos_albums"
+        self.driver.get(photos_id_url)
+        # Get scroll height
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+        while True:
+            # Scroll down to bottom
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # Wait to load page
+            time.sleep(PAUSE_TIME)
+            # Calculate new scroll height and compare with last scroll height
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                time.sleep(PAUSE_TIME)
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                if new_height == last_height:
+                    xpathroot = '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[3]'
+                    divs = self.driver.find_element(by=By.XPATH, value=xpathroot)
+                    element = divs.get_attribute('innerHTML')
+                    soup = BeautifulSoup(element,'html.parser')
+                    for elem in soup.find_all('div'):
+                        if elem.find('div'):
+                            if elem.find('div').find('a'):
+                                print(elem.find('div').find('a')['href'])
+                                titlelist = []
+                                for divelem in list(set(elem.find('div').find('a').find('div').find('div').find_all('div'))):
+                                    if(divelem.find('div')):
+                                        if(divelem.find('div').find('span')):
+                                            titlelist.append(divelem.find('div').find('span').text)
+                                self.get_album_photos(elem.find('div').find('a')['href'],str(list(set(titlelist))[0]),folderpath)
+                    break
+            last_height = new_height
     def scroll_page_photos_by(self, profile_id,downloadfolder):
         folderpath = f'{downloadfolder}{os.sep}{profile_id}{os.sep}photos_by'
         if not os.path.exists(folderpath):
@@ -118,11 +186,8 @@ if __name__ == '__main__':
     # Enter your login credentials here
     fb_login = FacebookLogin(email='email_id', password='password')
     fb_login.login()#Uncomment this for the first time. comment this after first run
-    profiles = ['<profile.ids>']
-for i in range(len(profiles)):
-    profile = profiles[i]
-
+    profile = input("Enter the profile id : ")
     fb_login.scroll_page_photos(profile,downloadfolder)
     fb_login.scroll_page_photos_by(profile,downloadfolder)
+    fb_login.scroll_page_photos_albums(profile,downloadfolder)
     os.system("taskkill /f /im chrome.exe")
-    #fb_login.scroll_page_photos_by('shradhanjali.rout.1',downloadfolder)
